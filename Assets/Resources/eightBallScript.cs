64b5678c0e9e2d6250376fcb7ba4e7ddd724b23d
kaowayne8 <wkao9@uic.edu>
2021-09-15 13:12:13 -0500

    Added 8 ball script

---
+++ Assets/Resources/eightBallScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class eightBallScript : MonoBehaviour
+{
+    public GameObject ballText;
+    int size;
+    string[] phrase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = 20;
+        phrase = new string[] 
+        {
+            "It is Certain",
+            "It is decidedly so",
+            "Without a doubt",
+            "Yes definitely",
+            "You may rely on it",
+            "As I see it, yes.",
+            "Most likely",
+            "Outlook good",
+            "Yes",
+            "Signs point to yes",
+            "Reply hazy, try again",
+            "Ask again later",
+            "Better not tell you now",
+            "Cannot predict now",
+            "Concentrate and ask again",
+            "Don't count on it",
+            "My reply is no",
+            "My sources say no",
+            "Outlook not so good",
+            "Very doubtful"
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Dot(transform.up, Vector3.down) > 0){
+            int num = Random.Range(0, size);
+            string randPhrase = phrase[num];  
+            ballText.GetComponent<TextMeshPro>().text = randPhrase;
+        }
+    }
+}
